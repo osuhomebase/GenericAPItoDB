1aa4fa85d0c6037da3e00b93725edd6adc450c48
osuhomebase <mbaron999@gmail.com>
2019-02-05 15:55:11 -0500

    added JavaScriptSerializerObjectExtensions.cs

---
+++ Api2db/JavaScriptSerializerObjectExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api2db
+{
+    public static class JavaScriptSerializerObjectExtensions
+    {
+        public static object JsonPropertyValue(this object obj, string key)
+        {
+            var dict = obj as IDictionary<string, object>;
+            if (dict == null)
+                return null;
+            object val;
+            if (!dict.TryGetValue(key, out val))
+                return null;
+            return val;
+        }
+
+        public static IEnumerable<string> JsonPropertyNames(this object obj)
+        {
+            var dict = obj as IDictionary<string, object>;
+            if (dict == null)
+                return null;
+            return dict.Keys;
+        }
+    }
+}
