d682d639127cd6c08882a4bab6cfce97f9940636
osuhomebase <mbaron999@gmail.com>
2019-02-04 22:23:59 -0500

    added Nuget Packages: * EntityFramework v6.2.0 * Microsoft.Extensions.Configuration v2.2.0 * Microsoft.Extensions.Configuration.Abstractions v2.2.0 * Microsoft.Extensions.Configuration.EnvironmentVariables v2.2.0 * Microsoft.Extensions.Configuration.FileExtensions v2.2.0 * Microsoft.Extensions.Configuration.Json v2.2.0 * Newtonsoft.Json v11.0.2 (Note: Don't update this or things will break)

--- Api2db/Api2db.cs
+++ Api2db/Api2db.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -5,31 +6,47 @@ using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Configuration;
 
 namespace Api2db
 {
     public static class Api2db
     {
         [FunctionName("Api2db")]
-        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequestMessage req, TraceWriter log)
+        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequestMessage req, TraceWriter log, ExecutionContext context)
         {
             log.Info("C# HTTP trigger function processed a request.");
 
-            // parse query parameter
-            string name = req.GetQueryNameValuePairs()
-                .FirstOrDefault(q => string.Compare(q.Key, "name", true) == 0)
-                .Value;
+            var resp = "";
 
-            if (name == null)
+            log.Info(context.FunctionAppDirectory);
+
+            var config = new ConfigurationBuilder()
+                 .SetBasePath(context.FunctionAppDirectory)
+                 .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+                 .AddEnvironmentVariables()
+                 .Build();
+
+            //log.Info(config["Values:NyuHousingAppsDbConn"]);
+            log.Info(config["ConnectionStrings:NyuHousingApps:ConnectionString"]);
+            return req.CreateResponse(HttpStatusCode.OK, "hi");
+
+            RoomLocationDbContext RoomLocations = new RoomLocationDbContext();
+            var listRoomLocations = RoomLocations.RoomLocations;
+            foreach(var roomLocation in listRoomLocations)
             {
-                // Get request body
-                dynamic data = await req.Content.ReadAsAsync<object>();
-                name = data?.name;
+                resp += roomLocation.Description += "<br />&nbsp;";
             }
 
-            return name == null
-                ? req.CreateResponse(HttpStatusCode.BadRequest, "Please pass a name on the query string or in the request body")
-                : req.CreateResponse(HttpStatusCode.OK, "Hello " + name);
+            return req.CreateResponse(HttpStatusCode.OK, resp);
+
+           
+        }
+
+        public static string GetEnvironmentVariable(string name)
+        {
+            return name + ": " +
+                System.Environment.GetEnvironmentVariable(name, EnvironmentVariableTarget.Process);
         }
     }
 }
