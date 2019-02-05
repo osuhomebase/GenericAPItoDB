6b0babd06aa15508cba31d55ed282d07a81683b8
osuhomebase <mbaron999@gmail.com>
2019-02-04 22:24:35 -0500

    added entity classes for testing - dbContext not able to get environment variable, however.

---
+++ Api2db/RoomLocationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data.Entity;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api2db
+{
+    class RoomLocationDbContext : DbContext
+    {
+        public RoomLocationDbContext() : base("name=NyuHousingApps") { }
+        public DbSet<RoomLocations> RoomLocations
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ Api2db/RoomLocations.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Api2db
+{
+    class RoomLocations
+    {
+        public int RoomLocationID { get; set; }
+        public string Description { get; set; }
+        public string Comments { get; set; }
+        public string City { get; set; }
+        public string StateProvince { get; set; }
+        public string ZipPostcode { get; set; }
+        public string CustomString1 { get; set; }
+
+    }
+}
