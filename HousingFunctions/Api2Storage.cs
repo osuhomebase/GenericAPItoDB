54340a98718e1babd3d15ae732d331c1c6a18383
thefifthisa <ijlavandero@gmail.com>
2019-02-25 14:53:23 -0500

    set parameters in query string, clean up try/catch logic

--- HousingFunctions/Api2Storage.cs
+++ HousingFunctions/Api2Storage.cs
@@ -22,65 +22,99 @@ namespace HousingFunctions
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
+            // set these parameters in query string (instead of as environment variables)
+            string apiUrl = req.Query["url"];
+            string azureWebJobsContainer = req.Query["container"];
+            string blockBlobFileName = req.Query["filename"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+
+            apiUrl = apiUrl ?? data?.url;
+            azureWebJobsContainer = azureWebJobsContainer ?? data?.container;
+            blockBlobFileName = blockBlobFileName ?? data?.filename;
+
+            if (apiUrl == null && azureWebJobsContainer == null)
+            {
+                return new BadRequestObjectResult("Please pass 'url' and 'container' (and optionally, 'filename') as parameters in the query string.");
+            }
+
             GenericAPIHelper APItest = new GenericAPIHelper(Environment.GetEnvironmentVariable("APIUsername", EnvironmentVariableTarget.Process), Environment.GetEnvironmentVariable("APIPassword", EnvironmentVariableTarget.Process));
-            object[] output = APItest.GetWebServiceResult(Environment.GetEnvironmentVariable("SampleAPIURL", EnvironmentVariableTarget.Process));
-            string[] keys;
-
-            string csvExport = "";
-
             IDynamicTable table = new DynamicTable(DynamicTableType.Expandable);
-            //IDynamicTable table = new DynamicTable(DynamicTableType.Expandable);
+            string csvExport = "";
 
             try
             {
+                // get api results
+                object[] output = APItest.GetWebServiceResult(apiUrl);
+
                 // get keys
-                keys = output[0].JsonPropertyNames().ToArray();
-                log.LogInformation(keys.Count().ToString());
+                string[] keys = output[0].JsonPropertyNames().ToArray();
+                // log.LogInformation(keys.Count().ToString());
+
                 // populate fields of each object and add to array
                 foreach (var row in output)
                 {
                     dynamic expando = new ExpandoObject();
                     foreach (var key in keys)
                     {
-                        log.LogInformation(key + "\t" + row.JsonPropertyValue(key));
                         ExpandoHelpers.AddProperty(expando, key, row.JsonPropertyValue(key));
+                        // log.LogInformation(key + "\t" + row.JsonPropertyValue(key));
                     }
                     table.AddRow(expando);
                 }
-                csvExport = table.AsCsv(true,',',true);
+                csvExport = table.AsCsv(true, ',', true);
             }
             catch (Exception ex)
             {
-                return new BadRequestObjectResult(ex.Message);
+                return new BadRequestObjectResult("Please pass valid 'url' as a parameter in the query string.");
             }
 
             string storageConnectionString = Environment.GetEnvironmentVariable("AzureWebJobsStorage");
-            //create a container CloudBlobContainer 
+
+            // create a container CloudBlobContainer 
             CloudStorageAccount storageAccount;
             CloudBlobClient cloudBlobClient;
 
-            // Check whether the connection string can be parsed.
+            // check whether the connection string can be parsed
             if (CloudStorageAccount.TryParse(storageConnectionString, out storageAccount))
             {
-                // If the connection string is valid, proceed with operations against Blob storage here.
+                // if the connection string is valid, proceed with operations against blob storage here
                 storageAccount = CloudStorageAccount.Parse(storageConnectionString);
                 cloudBlobClient = storageAccount.CreateCloudBlobClient();
-                //AzureWebJobsContainer is not created by default
-                CloudBlobContainer cloudBlobContainer = cloudBlobClient.GetContainerReference(Environment.GetEnvironmentVariable("AzureWebJobsContainer"));
 
-                // create blob and write to storage
-                CloudBlockBlob blockBlob = cloudBlobContainer.GetBlockBlobReference("test.csv");
-                await blockBlob.UploadTextAsync(csvExport);
+                CloudBlobContainer cloudBlobContainer;
+                try
+                {
+                    // note: azureWebJobsContainer is not created by default
+                    cloudBlobContainer = cloudBlobClient.GetContainerReference(azureWebJobsContainer);
+
+                    // create blob and write to storage
+                    if (blockBlobFileName == null)
+                    {
+                        blockBlobFileName = DateTime.Now.ToString("MM-dd-yyyy HH:mm:ss");
+                    }
+
+                    CloudBlockBlob blockBlob = cloudBlobContainer.GetBlockBlobReference(blockBlobFileName + ".csv");
+                    // log.LogInformation(blockBlobFileName + ".csv");
+
+                    await blockBlob.UploadTextAsync(csvExport);
+                }
+                catch
+                {
+                    return new BadRequestObjectResult("Please pass valid 'container' as a parameter in the query string.");
+                }
             }
             else
             {
-                // Otherwise, let the user know that they need to define the environment variable.
-                log.LogInformation(
+                // otherwise, let the user know that they need to define the environment variable
+                return new BadRequestObjectResult(
                     "A connection string has not been defined in the system environment variables. " +
-                    "Add a environment variable named 'storageconnectionstring' with your storage " +
+                    "Add a environment variable named 'AzureWebJobsStorage' with your storage " +
                     "connection string as a value.");
             }
-            return (ActionResult)new OkObjectResult($"Hello, thanks for playing!");
+
+            return new OkObjectResult($"Successful request! 'url' = {apiUrl}, 'container' = {azureWebJobsContainer}, 'filename' = {blockBlobFileName}");
         }
     }
 }
