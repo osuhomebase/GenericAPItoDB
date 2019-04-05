59664dd6b2f871749c4100d2c741bb4cb5450799
thefifthisa <ijlavandero@gmail.com>
2019-04-05 13:51:05 -0400

    parse json array of queries, store query results as csv in azure blob

--- HousingFunctions/OracleDb2SQLDb.cs
+++ HousingFunctions/OracleDb2SQLDb.cs
@@ -8,6 +8,11 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using Oracle.ManagedDataAccess.Client;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System.Data;
+using System.Text;
+using System.Collections.Generic;
 
 namespace HousingFunctions
 {
@@ -31,18 +36,120 @@ namespace HousingFunctions
                 return new BadRequestObjectResult("Please specify configuration name ('config') in the query string.");
             }
 
+            // parse json array
+            OracleDataFlow result;
+            Entry currConfig = null;
+            using (StreamReader r = new StreamReader("queries.json")) // in json properties, set Build Action to Content and Copy to Output Directory to Copy always
+            {
+                string json = r.ReadToEnd();
+                result = JsonConvert.DeserializeObject<OracleDataFlow>(json);
+            }
+
+            // find entry with specified config
+            foreach (Entry q in result.Queries)
+            {
+                if (q.ConfigurationName.Equals(configurationName))
+                {
+                    currConfig = q;
+                }
+            }
+
+            if (currConfig == null)
+            {
+                return new BadRequestObjectResult("Please specify configuration name and query as an entry in queries.json.");
+            }
+
             // connect to oracle db
             string OracleConnectionString = new OracleConnectionStringBuilder()
             {
+                // connection string had to be split into individual variables
                 DataSource = Environment.GetEnvironmentVariable("OracleDataSource", EnvironmentVariableTarget.Process),
                 UserID = Environment.GetEnvironmentVariable("OracleUserID", EnvironmentVariableTarget.Process),
                 Password = Environment.GetEnvironmentVariable("OraclePassword", EnvironmentVariableTarget.Process),
             }.ConnectionString;
 
-            OracleConnection conn = new OracleConnection(OracleConnectionString);
-            conn.Open();
+            using (OracleConnection conn = new OracleConnection(OracleConnectionString))
+            {
+                conn.Open();
+                using (OracleCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = currConfig.Query;
 
-            return new OkObjectResult("Connection established (" + conn.ServerVersion + ")");
+                    DataTable table = new DataTable();
+                    string csvExport = "";
+
+                    // run corresponding query, load results in data table and convert to csv
+                    using (OracleDataReader dr = cmd.ExecuteReader())
+                    {
+                        table.Load(dr);
+                        csvExport = DataTableToCSV(table);
+                    }
+
+                    string storageConnectionString = Environment.GetEnvironmentVariable("AzureWebJobsStorage", EnvironmentVariableTarget.Process);
+
+                    // create a container CloudBlobContainer 
+                    CloudStorageAccount storageAccount;
+                    CloudBlobClient cloudBlobClient;
+
+                    // check whether the connection string can be parsed
+                    if (CloudStorageAccount.TryParse(storageConnectionString, out storageAccount))
+                    {
+                        // if the connection string is valid, proceed with operations against blob storage here
+                        storageAccount = CloudStorageAccount.Parse(storageConnectionString);
+                        cloudBlobClient = storageAccount.CreateCloudBlobClient();
+
+                        CloudBlobContainer cloudBlobContainer = cloudBlobClient.GetContainerReference(Environment.GetEnvironmentVariable("AzureWebJobsContainer", EnvironmentVariableTarget.Process));
+                        CloudBlockBlob blockBlob = cloudBlobContainer.GetBlockBlobReference($"{currConfig.ConfigurationName} {DateTime.Now.ToString("MM-dd-yyyy HH:mm:ss")}.csv");
+
+                        // upload csv to blob
+                        await blockBlob.UploadTextAsync(csvExport);
+                    }
+                    else
+                    {
+                        // otherwise, let the user know that they need to define the environment variable
+                        return new BadRequestObjectResult(
+                            "A connection string has not been defined in the system environment variables. " +
+                            "Add a environment variable named 'AzureWebJobsStorage' with your storage " +
+                            "connection string as a value.");
+                    }
+                }
+
+                conn.Close();
+            }
+
+            return new OkObjectResult("Successful query!");
         }
+
+        public static string DataTableToCSV(DataTable table)
+        {
+            var result = new StringBuilder();
+            for (int i = 0; i < table.Columns.Count; i++)
+            {
+                result.Append(table.Columns[i].ColumnName);
+                result.Append(i == table.Columns.Count - 1 ? "\n" : "\t");
+            }
+
+            foreach (DataRow row in table.Rows)
+            {
+                for (int i = 0; i < table.Columns.Count; i++)
+                {
+                    result.Append(row[i].ToString());
+                    result.Append(i == table.Columns.Count - 1 ? "\n" : "\t");
+                }
+            }
+
+            return result.ToString();
+        }
+    }
+
+    public class Entry
+    {
+        public string ConfigurationName { get; set; }
+        public string Query { get; set; }
+    }
+
+    public class OracleDataFlow
+    {
+        public List<Entry> Queries { get; set; }
     }
 }
